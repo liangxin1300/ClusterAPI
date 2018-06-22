f00c599ea5f9d35e23f58485a5cc3cfa5537259b
liangxin1300 <XLiang@suse.com>
2018-06-22 11:03:38 +0800

    Add corosync.conf.template

--- corosync.py
+++ corosync.py
@@ -2,10 +2,16 @@ from flask_restful import Resource, reqparse
 
 parser = reqparse.RequestParser()
 parser.add_argument('port')
+parser.add_argument('bindnetaddr')
+parser.add_argument('ucastaddr')
+parser.add_argument('mcastaddr')
+
 
 class Corosync(Resource):
 
     def post(self):
         args = parser.parse_args()
-        print({'task': args['port']})
+        #print({'port': args['port']})
+        #print({'bindnetaddr': args['bindnetaddr']})
+        #print({'mcastaddr': args['mcastaddr']})
         return '', 201
