ffda5cdfc79aefc6665bac8c078c6c79e99e5c97
liangxin1300 <XLiang@suse.com>
2018-07-27 15:40:55 +0800

    change setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@
 from setuptools import setup, find_packages
 
 setup(name='ClusterAPI',
-      version='0.5.1',
+      version='0.6',
       author='Xin Liang',
       author_email='XLiang@suse.com',
       packages=find_packages(),
       scripts=['bin/clusterAPIServer'],
-      install_requires=['Flask', 'xmltodict', 'PyJWT', 'python-pam'])
+      install_requires=['Flask', 'flask_swagger','xmltodict', 'PyJWT', 'python-pam'])
